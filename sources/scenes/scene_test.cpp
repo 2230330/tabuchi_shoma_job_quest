6bdf3a8d964be8db7c39d5d15df812be7d5e96e7
2230330 <2230330@MSI>
2026-07-30 15:49:08 +0900

    不要オブジェクトの削除、初めに見てほしいものを修正

--- sources/scenes/scene_test.cpp
+++ sources/scenes/scene_test.cpp
@@ -57,7 +57,6 @@ bool SceneTest::InitializeCore()
         //3dオブジェクト宣言
         {
             ResourceManager::Instance().LoadGltfModel(device, ".\\resources\\model\\gltf\\DamagedHelmet\\DamagedHelmet.gltf");
-            ResourceManager::Instance().LoadGltfModel(device, ".\\resources\\model\\gltf\\blue_exagonal_tiles_with_extracted\\scene.gltf");
             ResourceManager::Instance().LoadGltfModel(device, ".\\resources\\model\\gltf\\cube.glb");
             ResourceManager::Instance().LoadGltfModel(device, ".\\resources\\model\\gltf\\plane.glb");
 
